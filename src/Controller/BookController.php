6aeb0bee0ebfbfb6a1835b86e6f325458228fb4d
RichardAbaleo <richard.abaleo.pro@gmail.com>
2023-01-03 14:32:02 +0100

    Controller + getAllBooks + getSpecificBook

---
+++ src/Controller/BookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'book', methods: ['GET'])]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $bookList = $bookRepository->findAll();
+        $jsonBookList = $serializer->serialize($bookList, 'json');
+
+        return new JsonResponse(
+            $jsonBookList,
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    #[Route('/api/books/{id}', name: 'specificBook', methods: ['GET'])]
+    public function getSpecificBook(Book $book, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonBook = $serializer->serialize($book, 'json');
+        return new JsonResponse(
+            $jsonBook,
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+}
